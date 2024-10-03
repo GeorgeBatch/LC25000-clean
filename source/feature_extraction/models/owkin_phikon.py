812fe3eb54ef29aa879d7ab181498c3fe5391293
George Batchkala <george.batchkala@gmail.com>
2024-10-03 16:54:11 +0100

    improve documentation

--- source/feature_extraction/models/owkin_phikon.py
+++ source/feature_extraction/models/owkin_phikon.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 from transformers import ViTModel, AutoModel
 
@@ -14,18 +15,22 @@ class OwkinPhikonFeatureExtractor(nn.Module):
 
     Version Details:
         - v1 (owkin/phikon):
-            - outputs: [batch_size, 197, 768]
-                - batch_size images
-                - 197 tokens = 1 cls token + 14*14 patch tokens
-                - 768 features
+            - outputs
+                - last_hidden_state [batch_size, 197, 768]
+                    - batch_size images
+                    - 197 tokens = 1 cls token + 14*14 patch tokens
+                    - 768 features
         - v2 (owkin/phikon-v2):
-            - outputs: [batch_size, 197, 1024]
-                - batch_size images
-                - 197 tokens = 1 cls token + 14*14 patch tokens
-                - 1024 features
+            - outputs
+                - pooler_output [batch_size, 1024] - the same as cls token at index 0
+                - last_hidden_state [batch_size, 197, 1024]
+                    - batch_size images
+                    - 197 tokens = 1 cls token + 14*14 patch tokens
+                    - 1024 features
     """
     def __init__(self, version):
         super().__init__()
+        self.version = version
 
         if version == "v1":
             self.model = ViTModel.from_pretrained(
@@ -38,5 +43,10 @@ class OwkinPhikonFeatureExtractor(nn.Module):
 
     def forward(self, x):
         outputs = self.model(x)
-        features = outputs.last_hidden_state[:, 0, :]
-        return features
+        
+        cls_token = outputs.last_hidden_state[:, 0, :]
+        # patch_tokens = outputs.last_hidden_state[:, 1:, :]
+        
+        # if self.version == "v2":
+        #     assert torch.equal(cls_token, outputs.pooler_output), "Pooler output and cls token should be the same for version={self.version}."
+        return cls_token
