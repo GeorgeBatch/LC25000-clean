1e8873d3606bad64043e597c34b4ae4466e1360c
George Batchkala <george.batchkala@gmail.com>
2024-10-16 15:23:12 +0100

    parametrize test using `ALL_EXTRACTOR_MODELS` instead of hardcoding the list of models

--- tests/unit/test_get_model_with_transform.py
+++ tests/unit/test_get_model_with_transform.py
@@ -1,22 +1,11 @@
 import pytest
 from torch import nn
 
+from source.constants import ALL_EXTRACTOR_MODELS
 from source.feature_extraction.get_model_with_transform import get_feature_extractor
 
 
-@pytest.mark.parametrize("model_name", [
-    # natural image models
-    'imagenet_resnet18-last-layer',
-    'imagenet_resnet50-clam-extractor',
-    'dinov2_vits14',
-    'dinov2_vitb14',
-    # pathology-specific models
-    'UNI',
-    'prov-gigapath',
-    'owkin-phikon',
-    'simclr-tcga-lung_resnet18-2.5x',
-    'simclr-tcga-lung_resnet18-10x',
-])
+@pytest.mark.parametrize("model_name", ALL_EXTRACTOR_MODELS)
 def test_get_feature_extractor(model_name):
     extractor = get_feature_extractor(model_name)
     assert isinstance(extractor, nn.Module)
