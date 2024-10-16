6956a9da38ba2ca810ee71911f7bfc6635d8a4ac
George Batchkala <george.batchkala@gmail.com>
2024-10-16 17:03:28 +0100

    scope should be "session" for both `mock_img_dir` and `sample_image_path` pytest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,14 @@
+import os
 import pytest
 
 # -------------------------------------------------------------------------------------
 # Fixtures
 # -------------------------------------------------------------------------------------
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def mock_img_dir():
     return "assets/test_images"
 
 @pytest.fixture(scope="session")
-def sample_image_path():
-    return f"{mock_img_dir()}/lungaca1.jpeg"
+def sample_image_path(mock_img_dir):
+    return os.path.join(mock_img_dir, "lungaca1.jpeg")
