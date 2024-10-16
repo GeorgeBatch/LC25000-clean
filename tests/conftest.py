4580aace83e41593fadc48ee944335c969903032
George Batchkala <george.batchkala@gmail.com>
2024-10-16 16:42:11 +0100

    move sample image to `assets/test_images/`

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,10 @@ import pytest
 # Fixtures
 # -------------------------------------------------------------------------------------
 
+@pytest.fixture
+def mock_img_dir():
+    return "assets/test_images"
+
 @pytest.fixture(scope="session")
 def sample_image_path():
-    return "assets/lungaca1.jpeg"
+    return f"{mock_img_dir()}/lungaca1.jpeg"
